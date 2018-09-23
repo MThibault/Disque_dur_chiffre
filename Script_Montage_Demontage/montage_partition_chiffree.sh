a39567569e9c67641b918144937baebde548a87c
MThibault <tmillant@gmail.com>
2018-09-24 00:12:58 +0200

    Translation in English and support of the Storage partition

--- Script_Montage_Demontage/montage_partition_chiffree.sh
+++ Script_Montage_Demontage/montage_partition_chiffree.sh
@@ -1,38 +1,66 @@
-#################################################################
-#	Script de montage d'une partition chiffrée		#
-#		Dev par Thibault MILLANT			#
-#################################################################
+#!/usr/bin/env bash
+
+#########################################################
+#	Script to mount the encrypted partition		#
+#########################################################
+
+#Author : Thibault MILLANT
 
 ## CHANGELOG :
+## - Storage partition support added
+## - Translating the code from french to english
 ## - Changing the code with Nextcloud instead of Owncloud
 
-echo "Il faut au minimum donner en paramètre la partition concernée sous la forme /dev/xxxx."
-if [ -z $1 ]; then # Correspond à la partition à utiliser
-	echo "Vous n'avez pas saisi de partition à monter en tant que premier paramètre."
-	exit
-fi
+#########################
+#	Function	#
+#########################
+function choose_mount() {
+	echo "1 - Mount the partition for Owncloud/Nextcloud"
+	echo "2 - Mount a storage partition"
+	echo "3 - Manual mounting"
+	read -p "Select the number corresponding to your solution you want to use : " chosen_mount
+	echo ""
+}
 
-read -p "Voulez vous monter la partition de stockage d'Owncloud/Nextcloud (yes/no) : " choix
-if [ $choix = "yes" ]; then
+
+## Start Script
+echo "At least, you need to send to the script, as first parameter, the partition you want to set up (example : /dev/sdb1)."
+if [ -z "$1" ]; then # Partition to use
+	echo "You forgot to enter the parameter related to the partition when you called the script (first parameter)."
+	exit 1
+fi
+echo ""
+
+choose_mount
+case "$chosen_mount" in
+1) 
 	cryptsetup luksOpen $1 Nextcloud_storage_encrypted
-	mkdir -p /media/Millant_1To_Io_N/Nextcloud_storage
-	mount -t ext4 /dev/mapper/Nextcloud_storage_encrypted /media/Millant_1To_Io_N/Nextcloud_storage
-	chown www-data:www-data -R /media/Millant_1To_Io_N/Nextcloud_storage/
-elif [ $choix = "no" ]; then
-	echo "Dans ce cas il vous faut aussi entrer le nom de la partition chiffrée, puis le point de montage."
-	if [ -z $2 ]; then # Correspond au nom de la partition chiffrée
-		echo "Vous n'avez pas saisi de nom en tant que second paramètre."
-		exit
+	mkdir -p /media/WebRPiservices_storage
+	mount -t ext4 /dev/mapper/WebRPiServices_storage_encrypted /media/WebRPiServices_storage/
+	chown www-data:www-data -R /media/WebRPiServices_storage
+	;;
+2)
+	cryptsetup luksOpen $1 StorageRPi_storage_encrypted
+	mkdir -p /media/StorageRPi_storage
+	mount -t ext4 /dev/mapper/StorageRPi_storage_encrypted /media/StorageRPi_storage/
+	chown www-data:www-data -R /media/StorageRPi_storage
+	;;
+3)
+	echo "You need also to send, as script parameters, the name you want to give to the mapper for the encrypted partition (without spaces), and then the path to the mounting point."
+	if [ -z "$2" ]; then # Encrypted partition name (mapper)
+		echo "You did not gave a name for the encrypted partition."
+		exit 1
 	fi
 
-	if [ -z $3 ]; then # Correspond au point de montage
-		echo "Vous n'avez pas saisie de point de montage en tant que troisième paramètre."
-		exit
+	if [ -z "$3" ] && [ ! -s "$3" ]; then # Correspond au point de montage
+		echo "You did not gave a path for the mounting point or the path is not valid."
+		exit 1
 	fi
 
 	cryptsetup luksOpen $1 $2
 	mkdir -p $3
 	mount -t ext4 /dev/mapper/$2 $3
-else
-	echo "Votre choix ne correspond pas aux choix attendus !"
-fi
+	;;
+*)
+	echo "Error in your selection."
+esac
